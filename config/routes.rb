0f19642ed1cf32ecdcff52c427ed418d0d4f0675
abrormukimov <abrormm34@gmail.com>
2022-09-09 12:29:28 +0500

    Update facebook api

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  authenticated :user do
+    root "home#index", as: 'authenticated_root'
+  end
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+end
